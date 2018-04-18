b90d76d0d33dd25bb7119a6f3c22cfcb83241da2
nelson119 <nelson119@outlook.com>
2018-04-18 11:24:23 +0000

    Deployed to Github Pages

---
+++ Plugin/plug_clock/js/clock.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	function rotateHands(element, deg){
+	  element.style.webkitTransform = 'rotate('+deg+'deg)';
+	  element.style.mozTransform    = 'rotate('+deg+'deg)';
+	  element.style.msTransform     = 'rotate('+deg+'deg)';
+	  element.style.oTransform      = 'rotate('+deg+'deg)';
+	  element.style.transform       = 'rotate('+deg+'deg)';
+	}
+
+	function updateTime(){
+	  var currentTime = new Date();
+		var hrs = currentTime.getHours();
+		var realtime = moment().format('YYYY/MM/DD, hh:mm:ss a').toUpperCase();
+		$('.location-map .desc p').html(realtime);
+	  if(hrs>12){
+	    hrs = hrs-12;
+	  }
+	  rotateHands(document.getElementById('secondHand'), currentTime.getSeconds()*6 );
+	  rotateHands(document.getElementById('minuteHand'), currentTime.getMinutes()*6 );
+	  rotateHands(document.getElementById('hourHand'), (hrs*30)+(currentTime.getMinutes()*6)/12);
+	}
+
+	if(document.getElementById('secondHand')!=null && 
+			document.getElementById('minuteHand')!=null && 
+			document.getElementById('hourHand')!=null){
+		updateTime();
+		window.setInterval(updateTime, 1000);
+	}
+	
+
+});
